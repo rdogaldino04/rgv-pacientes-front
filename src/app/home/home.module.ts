6bf08574419831b2bc9e0e1ba36f29311e6765c2
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-08-20 13:08:17 -0300

    feat: create component home

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home.routing.module';
+
+@NgModule({
+    declarations: [         
+        HomeComponent
+    ],
+    imports: [ 
+        CommonModule,
+        RouterModule,
+        HomeRoutingModule
+    ],    
+})
+export class HomeModule { }
