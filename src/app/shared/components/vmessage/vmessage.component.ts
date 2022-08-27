1a4d89b88716d915bc8192301008238af026f293
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-08-26 23:47:32 -0300

    fix lint

--- src/app/shared/components/vmessage/vmessage.component.ts
+++ src/app/shared/components/vmessage/vmessage.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'ap-vmessage',
+    selector: 'app-vmessage',
     templateUrl: './vmessage.component.html'
 })
 export class VMessageComponent {
