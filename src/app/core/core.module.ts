ab384357c8a9928c5721594af7a5848094f73fca
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-08-26 23:46:29 -0300

    feat: add component alert

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { AlertModule } from '../shared/components/alert/alert.module';
 
 @NgModule({
     declarations: [HeaderComponent],
     exports: [HeaderComponent],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        AlertModule
     ],
 
 })
