51404197c575daa0c1d9ec67d85926e6ac2d2058
rdogaldino04 <rdogaldino04@gmail.com>
2024-05-15 10:32:03 -0300

    organize patient filter forms

--- src/app/modules/cadastre/patients/patient-filter/patient-filter.module.ts
+++ src/app/modules/cadastre/patients/patient-filter/patient-filter.module.ts
@@ -4,18 +4,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PatientFilterComponent } from './patient-filter.component';
 import { AppMaterialModule } from 'src/app/shared/app-material/app-material.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-    declarations: [PatientFilterComponent],
-    exports: [PatientFilterComponent],
-    imports: [
-        CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-        AppMaterialModule,
-        SharedModule
-    ],
+  declarations: [PatientFilterComponent],
+  exports: [PatientFilterComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppMaterialModule,
+    SharedModule,
+    FlexLayoutModule,
+  ],
 })
-export class PatientFilterModule {
-
-}
+export class PatientFilterModule {}
