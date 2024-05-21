51b3d4f7fefdcc170149caeefef1720ecc2b5206
rdogaldino04 <rdogaldino04@gmail.com>
2024-05-21 14:24:28 -0300

    feat: delete batch

--- src/app/modules/cadastre/batch/batch.component.ts
+++ src/app/modules/cadastre/batch/batch.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { PageEvent } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { BatchPage } from 'src/app/model/batch-page';
@@ -17,7 +18,8 @@ export class BatchComponent implements OnInit {
   constructor(
     private batchService: BatchService,
     private formBuilder: UntypedFormBuilder,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -50,7 +52,20 @@ export class BatchComponent implements OnInit {
   }
 
   onDelete(id: number) {
-    this.batchService.delete(id).subscribe(() => this.onFilter());
+    this.batchService.delete(id).subscribe(
+      () => {
+        this.onFilter();
+      },
+      (e) => {
+        if (e?.status === 400 || e?.status === 404) {
+          this.snackBar.open(e?.error?.userMessage, '', { duration: 5000 });
+        } else {
+          this.snackBar.open('Erro ao excluir o lote', '', {
+            duration: 5000,
+          });
+        }
+      }
+    );
   }
 
   onPageInfo(pageEvent: PageEvent): void {
