46c6b78cac96019f5df46f8f0d56a534d70e6485
rdogaldino04 <rdogaldino04@gmail.com>
2024-06-08 08:43:47 -0300

    otimizar module remove imports desnecessarios

--- src/app/modules/cadastre/cadastre.module.ts
+++ src/app/modules/cadastre/cadastre.module.ts
@@ -1,12 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ProductModule } from './product/product.module';
 import { CadastreRoutingModule } from './cadastre.routing.modeule';
-import { BatchModule } from './batch/batch.module';
 
 @NgModule({
   declarations: [],
 
-  imports: [CommonModule, CadastreRoutingModule, ProductModule, BatchModule],
+  imports: [CommonModule, CadastreRoutingModule],
 })
 export class CadastreModule {}
