301163701618c5ab2783e01045219b58a0c09eac
rdogaldino04 <rdogaldino04@gmail.com>
2024-06-08 09:45:00 -0300

    cria SectorListComponent

---
+++ src/app/modules/cadastre/sector/sector-list/sector-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sector-list',
+  templateUrl: './sector-list.component.html',
+  styleUrls: ['./sector-list.component.scss']
+})
+export class SectorListComponent {
+
+}
--- src/app/modules/cadastre/sector/sector.module.ts
+++ src/app/modules/cadastre/sector/sector.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectorComponent } from './sector.component';
 import { SectorRoutingModule } from './sector.routing.module';
+import { SectorListComponent } from './sector-list/sector-list.component';
 
 @NgModule({
-  declarations: [SectorComponent],
+  declarations: [SectorComponent, SectorListComponent],
   imports: [CommonModule, SectorRoutingModule],
 })
 export class SectorModule {}
